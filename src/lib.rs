4014ef86249a9226a9ea254228a94624fc79df51
Fanda Vacek <fanda.vacek@gmail.com>
2022-01-03 23:37:51 +0100

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -249,7 +249,7 @@ impl BarebonesWatchFace {
         let mut buf = new_string();
         write!(
             &mut buf,                    //  Write the formatted text
-            "{} {}%{}#\nFanda ({}mV)\0",  //  Must terminate Rust strings with null
+            "{} {}%{}#\nFANDA ({}mV)\0",  //  Must terminate Rust strings with null
             color,
             percentage,
             symbol,
